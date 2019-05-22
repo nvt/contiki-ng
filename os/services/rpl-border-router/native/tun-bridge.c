b7323396971f17cb7830b41eadfa92be10a13121
Nicolas Tsiftes <nicolas.tsiftes@ri.se>
2019-05-22 17:01:53 +0200

    Ensure that the interface name gets null-terminated.

--- os/services/rpl-border-router/native/tun-bridge.c
+++ os/services/rpl-border-router/native/tun-bridge.c
@@ -165,7 +165,8 @@ tun_alloc(char *dev)
    */
   ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
   if(*dev != 0) {
-    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ - 1);
+    ifr.ifr_name[IFNAMSIZ - 1] = '\0';
   }
 
   if((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0) {
