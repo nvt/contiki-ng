4b8a121033d5ecf8b4e394736a32887f66e442a0
Nicolas Tsiftes <nicolas.tsiftes@ri.se>
2024-07-11 15:59:04 +0200

    Update IPSO objects example with DTLS information and configuration.

--- examples/lwm2m-ipso-objects/project-conf-basic.h
+++ examples/lwm2m-ipso-objects/project-conf-basic.h
@@ -27,44 +27,11 @@
  * SUCH DAMAGE.
  */
 
-#ifndef PROJECT_CONF_H_
-#define PROJECT_CONF_H_
+#ifndef PROJECT_CONF_BASIC_H
+#define PROJECT_CONF_BASIC_H
 
-#ifdef BOARD_STRING
-#define LWM2M_DEVICE_MODEL_NUMBER BOARD_STRING
-#elif defined(CONTIKI_TARGET_WISMOTE)
-#include "dev/watchdog.h"
-#define LWM2M_DEVICE_MODEL_NUMBER "LWM2M_DEVICE_MODEL_NUMBER"
-#define LWM2M_DEVICE_MANUFACTURER "LWM2M_DEVICE_MANUFACTURER"
-#define LWM2M_DEVICE_SERIAL_NO    "LWM2M_DEVICE_SERIAL_NO"
-#define PLATFORM_REBOOT watchdog_reboot
-#endif
+#include "project-conf-common.h"
 
-#if BOARD_SENSORTAG
-/* Real sensor is present... */
-#else
-#define IPSO_TEMPERATURE example_ipso_temperature
-#endif /* BOARD_SENSORTAG */
+#define LWM2M_ENGINE_CLIENT_ENDPOINT_NAME "Contiki-NG-IPSO"
 
-/* Increase rpl-border-router IP-buffer when using more than 64. */
-#define COAP_MAX_CHUNK_SIZE            64
-
-/* Multiplies with chunk size, be aware of memory constraints. */
-#define COAP_MAX_OPEN_TRANSACTIONS     4
-
-/* Filtering .well-known/core per query can be disabled to save space. */
-#define COAP_LINK_FORMAT_FILTERING     0
-#define COAP_PROXY_OPTION_PROCESSING   0
-
-/* Enable client-side support for COAP observe */
-#define COAP_OBSERVE_CLIENT 1
-
-/* Definitions to enable Queue Mode, include the dynamic adaptation and change the default parameters  */
-/* #define LWM2M_QUEUE_MODE_CONF_ENABLED 1
-   #define LWM2M_QUEUE_MODE_CONF_INCLUDE_DYNAMIC_ADAPTATION 1
-   #define LWM2M_QUEUE_MODE_CONF_DEFAULT_CLIENT_AWAKE_TIME 2000
-   #define LWM2M_QUEUE_MODE_CONF_DEFAULT_CLIENT_SLEEP_TIME 10000
-   #define LWM2M_QUEUE_MODE_CONF_DEFAULT_DYNAMIC_ADAPTATION_FLAG 0
-   #define LWM2M_QUEUE_MODE_OBJECT_CONF_ENABLED 1 */
-
-#endif /* PROJECT_CONF_H_ */
+#endif /* PROJECT_CONF_BASIC_H */
